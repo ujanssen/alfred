3d40a2dc96ce0d7f5339cdd0e2c2ab581afa02a8
Uwe Janssen <mail@uwe-janssen.de>
2014-05-06 06:17:42 +0200

    logging removed

--- cputemp-monitor/cputemp-monitor.rb
+++ cputemp-monitor/cputemp-monitor.rb
@@ -4,6 +4,5 @@ while true do
   out = `/usr/bin/sensors`.split[5]
   out = out[0..-2]
  `/usr/bin/gmetric --name=cpu_temp --type=float --group=cpu --value=#{out.to_f} --units=C --tmax=90 --dmax=600`
-  puts "/usr/bin/gmetric --name=cpu_temp --type=float --group=cpu --value=#{out.to_f} --units=C --tmax=90 --dmax=600"
   sleep 15.0
 end

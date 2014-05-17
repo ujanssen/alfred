a9c18a8d042daa2b6f7fc858b5018f806498e161
Uwe Janßen <uwe.janssen@devbliss.com>
2014-05-18 00:09:37 +0200

    Update hddtemp-monitor.rb

--- hddtemp-monitor/hddtemp-monitor.rb
+++ hddtemp-monitor/hddtemp-monitor.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 
 while true do
-  temp = `/usr/sbin/hddtemp -n /dev/sda`.strip.to_f
- `/usr/bin/gmetric --name=VB0250EAVER_Z3TCKR9Q_temp --type=float --group=hddtemp --value=#{temp} --units=C --tmax=90 --dmax=600`
+  temp = `/usr/sbin/hddtemp -n /dev/sdc`.strip.to_f
+ `/usr/bin/gmetric --name=Samsung_SSD_840_temp --type=float --group=hddtemp --value=#{temp} --units=C --tmax=90 --dmax=600`
   sleep 5.0
 
-  temp = `/usr/sbin/hddtemp -n /dev/sdb`.strip.to_f
+  temp = `/usr/sbin/hddtemp -n /dev/sda`.strip.to_f
  `/usr/bin/gmetric --name=WD30EFRX_T13732_temp --type=float --group=hddtemp --value=#{temp} --units=C --tmax=90 --dmax=600`
   sleep 5.0
   
-  temp = `/usr/sbin/hddtemp -n /dev/sdc`.strip.to_f
+  temp = `/usr/sbin/hddtemp -n /dev/sdb`.strip.to_f
  `/usr/bin/gmetric --name=WD30EFRX_T03584_temp --type=float --group=hddtemp --value=#{temp} --units=C --tmax=90 --dmax=600`
   sleep 5.0
 end

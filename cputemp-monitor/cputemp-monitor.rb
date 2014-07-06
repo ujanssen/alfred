63b1fe26bde0c6763ddb4c0c293086e4f034308c
Uwe Janßen <uwe.janssen@devbliss.com>
2014-07-06 12:49:48 +0200

    Update cputemp-monitor.rb

--- cputemp-monitor/cputemp-monitor.rb
+++ cputemp-monitor/cputemp-monitor.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby 
 
 while true do
-  out = `/usr/bin/sensors`.split[5]
+  out = `/usr/bin/sensors`.split[5] # 21 on brix
   out = out[0..-2]
  `/usr/bin/gmetric --name=cpu_temp --type=float --group=cpu --value=#{out.to_f} --units=C --tmax=90 --dmax=600`
   sleep 15.0
